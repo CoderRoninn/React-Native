8219656eb563db6a1edadca9e757fad9f30a0728
Doğukan Süme <161054688+CoderRoninn@users.noreply.github.com>
2025-07-06 22:25:40 +0300

    Delete Tasks directory

--- Tasks/Task1/welcome.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-import styles from "./style";
-
-const Welcome = () => {
-  return <Text style={styles.text}>Welcome Our App.</Text>;
-};
-export default Welcome;
